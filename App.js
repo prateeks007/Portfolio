199a80a73b93b6daeba458172e846ab01958866e
Prateek Shetty <prateekshettybdmi@gmail.com>
2023-09-27 01:25:21 +0530

    check push

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MainScreen from './MainScreen';
+import MainScreen from "./MainScreen";
 
 export default function App() {
-  return (
-    <MainScreen></MainScreen>
-    
-  );
+  return <MainScreen></MainScreen>;
 }
-
